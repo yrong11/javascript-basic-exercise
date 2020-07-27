2e255c66138ec44bed4407229676964b70dd392d
yurong <rong.yu@thoughtworks.com>
2020-07-27 16:05:54 +0800

    fix: exercise_03 rangeAndSum

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,8 +6,10 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  const result = [];
+  if (start <= end) for (let i = start; i < end; i += 1) result.push(i);
+  else for (let i = start; i > end; i -= 1) result.push(i);
+  return result;
 }
 
 export function sum(...numbers) {
@@ -17,6 +19,8 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (numbers === undefined) return 0;
+  let result = 0;
+  for (let i = 0; i < numbers.length; i += 1) result += numbers[i];
+  return result;
 }
