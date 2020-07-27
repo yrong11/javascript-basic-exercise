9e8c6d26d93749be5db03c7a724a9dea598543d8
yurong <rong.yu@thoughtworks.com>
2020-07-27 15:50:43 +0800

    fix: exercise_02 src

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,16 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
-
+  let len = 0;
+  if (string == null || string == undefined) return 0;
+  if (prediction != undefined) {
+    for (let i = 0; i < string.length; i += 1) {
+      if (prediction(string[i])) {
+        len += 1;
+      }
+    }
+    return len;
+  }
+  return string.length;
   throw new Error('Please delete this line and implement the function');
 }
