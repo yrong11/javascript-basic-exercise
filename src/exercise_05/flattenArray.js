dc3833cb4a92feff2fe548da0b33927fb27f3fda
yurong <rong.yu@thoughtworks.com>
2020-07-28 15:53:35 +0800

    fix: exercise_05 flattenArray

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,14 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  try {
+    const result = [];
+    for (let i = 0; i < array.length; i += 1) {
+      if (!(array[i] instanceof Array)) result.push(array[i]);
+      else for (let j = 0; j < array[i].length; j += 1) result.push(array[i][j]);
+    }
+    return result;
+  } catch (err) {
+    throw new Error('Flatten undefined or null array');
+  }
 }
