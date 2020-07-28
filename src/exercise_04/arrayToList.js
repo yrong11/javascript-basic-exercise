a9dd891437650a5088cf9f9a2228e773a7d13f4b
yurong <rong.yu@thoughtworks.com>
2020-07-28 16:56:11 +0800

    fix: exercise_04 arraytoList

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,24 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
+  function Node(value = 0, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 
-  throw new Error('Please delete this line and implement the function');
+  try {
+    if (array.length == 0) throw new Error('Creating list from empty array');
+    const first = new Node(array[0]);
+    let pre = first;
+    for (let i = 1; i < array.length; i += 1) {
+      const node = new Node(array[i]);
+      pre.next = node;
+      pre = node;
+    }
+    return first;
+  } catch (err) {
+    if (array == undefined || array === null) {
+      throw new Error('Creating list from undefined array');
+    } else throw new Error('Creating list from empty array');
+  }
 }
